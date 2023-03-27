c07e1af178b109781b8ca8b0276c32851c9ad5df
ECorvi0 <48996058+ECorvi0@users.noreply.github.com>
2023-03-27 09:03:48 +0200

    Add files via upload
    Résolution des bugs de l'interface (html et js) et style appliqué sur la taille de la carte dans la page

--- Client/main.js
+++ Client/main.js
@@ -5,17 +5,17 @@ window.onload = function () {
     L.tileLayer(
         tileGeoportailWMTS,
         {
-            minZoom: 0,
+            minZoom: 8,
             maxZoom: 18,
             tileSize: 256,
             attribution: "IGN-F/Géoportail"
         }).addTo(mymap);
 }
 
-//acces aux donnees
+// Accès aux données carroyées
 
-const grid = "/Projet BANO/Serveur/Compte_adresses_67.geojson";
-const bufferedGrid = "/Projet BANO/Serveur/buffered_Compte_adresses_67.geojson";
+const grid = "/Projet_BANO/Serveur/Compte_adresses_67.geojson";
+const bufferedGrid = "/Projet_BANO/Serveur/buffered_Compte_adresses_67.geojson";
 
 //fonction de coloration des données carroyées pour faciliter la visualisation
 
@@ -29,8 +29,10 @@ var g = 255;
 var b = 0;
 
 function getShadedColor(n) {
-    return "rgba(" + r * n + "," + g * n ** 0.5 + "," + b * n + ",1)";
+    return "rgba(" + r * n + "," + g * n + "," + b * n + ",1)";
 }
+
+// Coloration binaire
 
 function binaryStyle(feature) {
     return {
@@ -43,6 +45,8 @@ function binaryStyle(feature) {
     };
 }
 
+// Coloration graduelle
+
 function shadedStyle(feature) {
     return {
         fillColor: getShadedColor(feature.properties.NUMPOINTS / feature.properties.ind_c * (1 - document.getElementById('buffer').checked / 2)),
@@ -54,13 +58,39 @@ function shadedStyle(feature) {
     };
 }
 
+/*function toLoadGrid(inputGrid) {
+    var outputGrid = L.geoJSON();
+
+    fetch(inputGrid)
+        .then(response => response.json())
+        .then(data => {
+            data.features.forEach(feature => {
+                const bounds = L.latLngBounds(feature.geometry.coordinates);
+                const mapBounds = L.latLngBounds(L.latLng([mapid.offsetTop, mapid.offsetLeft]), L.latLng([mapid.offsetTop + mapid.offsetHeight, mapid.offsetLeft + mapid.offsetWidth]));
+                if (bounds.intersects(mapBounds)) {
+                    emptyGeojson.addData(feature);
+                }
+            });
+
+            outputGrid.addTo(map);
+        });
+
+    return outputGrid
+}*/
+
+var mapGrid = L.layerGroup()
+
+//permet de vérifier visuellement l'implémentation des données carroyées
 async function getGrid() {
-    //permet de vérifier visuellement l'implémentation des données carroyées
-    const response = await fetch(grid);
-    const data = await response.json();
-    var geoJsonGrid = L.geoJson(data);
+    // Initialisation de la grille sans buffer dans la fonction
+    if (document.getElementById('noBuffer').checked) {
+        const response = await fetch(grid);
+        const data = await response.json();
+        var geoJsonGrid = L.geoJson(data);
+    }
     //Vérifie si le bouton buffer est coché
-    if (document.getElementById('buffer').checked) {
+    else if (document.getElementById('buffer').checked) {
+        // Récupération de la grille avec buffer
         const bufferedResponse = await fetch(bufferedGrid);
         const bufferedData = await bufferedResponse.json();
         geoJsonGrid = L.geoJson(bufferedData);
@@ -70,15 +100,31 @@ async function getGrid() {
         geoJsonGrid = leafletGridBuffered;
     }
     if (document.getElementById('gradient').checked) {
-        L.geoJSON(geoJsonGrid.toGeoJSON(), { style: shadedStyle }).addTo(mymap);
+        L.geoJSON(geoJsonGrid.toGeoJSON(), { style: shadedStyle }).addTo(mapGrid);
     }
     else if (document.getElementById('binary').checked) {
-        L.geoJSON(geoJsonGrid.toGeoJSON(), { style: binaryStyle }).addTo(mymap);
+        L.geoJSON(geoJsonGrid.toGeoJSON(), { style: binaryStyle }).addTo(mapGrid);
     }
+    mapGrid.addTo(mymap)
 }
 
+function clearAllLayers() {
+    for (var i = 0; i < mapGrid.length; i++) {
+        mymap.removeLayer(mapGrid[i]);
+    }
+    //this line empties the array
+    mapGrid.length = 0;
+}
+
+mymap.on('click', function () {
+    clearAllLayers()
+    //mymap.removeLayer(mapGrid);
+    //var mapGrid = L.layerGroup()
+    getGrid()
+});
+
 var radiosOui = document.getElementsByName('ouinon');
-var radiosEval = document.getElementsByName('eval');
+var radiosMode = document.getElementsByName('mode');
 for (i = 0; i < radiosOui.length; i++) {
     radiosOui[i].onclick = function (e) {
         // un clic sur un bouton coché tout en maintenant la touche ctrl le décoche
@@ -87,13 +133,8 @@ for (i = 0; i < radiosOui.length; i++) {
         }
     }
 }
-for (i = 0; i < radiosEval.length; i++) {
-    radiosEval[i].onload = function (e) {
-        if (e.value == 1) {
-            this.checked = true;
-        }
-    }
-    radiosEval[i].onclick = function (e) {
+for (i = 0; i < radiosMode.length; i++) {
+    radiosMode[i].onclick = function (e) {
         // un clic sur un bouton coché tout en maintenant la touche ctrl le décoche
         if (e.ctrlKey || e.metaKey) {
             this.checked = false;
@@ -101,4 +142,4 @@ for (i = 0; i < radiosEval.length; i++) {
     }
 }
 
-getGrid();
+//toLoadGrid(grid);
