402a41f8ab46480ed58105df5aeb34d2a5381dd6
ECorvi0 <48996058+ECorvi0@users.noreply.github.com>
2023-04-03 12:10:24 +0200

    Add files via upload
    Mise à jour du Javascript, débarrassé des bugs qui pouvaient exister à l'intérieur

--- Client/main.js
+++ Client/main.js
@@ -1,16 +1,16 @@
 // creation de la carte
-var mymap = L.map("mapid").setView([48.5, 7.5], 12);
+var mymap = L.map("mapid").setView([48.5, 7.5], 14);
 const tileGeoportailWMTS = 'https://wxs.ign.fr/essentiels/geoportail/wmts?service=WMTS&request=GetTile&version=1.0.0&tilematrixset=PM&tilematrix={z}&tilecol={x}&tilerow={y}&layer=ORTHOIMAGERY.ORTHOPHOTOS&format=image/jpeg&style=normal'
-window.onload = function () {
-    L.tileLayer(
-        tileGeoportailWMTS,
-        {
-            minZoom: 8,
-            maxZoom: 18,
-            tileSize: 256,
-            attribution: "IGN-F/Géoportail"
-        }).addTo(mymap);
-}
+
+L.tileLayer(
+    tileGeoportailWMTS,
+    {
+        minZoom: 8,
+        maxZoom: 18,
+        tileSize: 256,
+        attribution: "IGN-F/Géoportail"
+    }).addTo(mymap);
+
 
 // Accès aux données carroyées
 
@@ -58,70 +58,52 @@ function shadedStyle(feature) {
     };
 }
 
-/*function toLoadGrid(inputGrid) {
-    var outputGrid = L.geoJSON();
+// Style de la grille, qui reprend les deux styles qui peuvent lui être appliqué selon ce que l'utilisateur choisit
 
-    fetch(inputGrid)
-        .then(response => response.json())
-        .then(data => {
-            data.features.forEach(feature => {
-                const bounds = L.latLngBounds(feature.geometry.coordinates);
-                const mapBounds = L.latLngBounds(L.latLng([mapid.offsetTop, mapid.offsetLeft]), L.latLng([mapid.offsetTop + mapid.offsetHeight, mapid.offsetLeft + mapid.offsetWidth]));
-                if (bounds.intersects(mapBounds)) {
-                    emptyGeojson.addData(feature);
-                }
-            });
+function mapStyle(feature) {
+    if (document.getElementById('binary').checked) {
+        return binaryStyle(feature)
+    }
+    else if (document.getElementById('gradient').checked) {
+        return shadedStyle(feature)
+    }
+}
 
-            outputGrid.addTo(map);
-        });
+// Initialisation de la grille
+var geoJsonGrid = L.geoJSON(null, { style: mapStyle }).addTo(mymap)
 
-    return outputGrid
-}*/
-
-var mapGrid = L.layerGroup()
 
 //permet de vérifier visuellement l'implémentation des données carroyées
 async function getGrid() {
-    // Initialisation de la grille sans buffer dans la fonction
+    // Récupération de la grille sans buffer
     if (document.getElementById('noBuffer').checked) {
-        const response = await fetch(grid);
-        const data = await response.json();
-        var geoJsonGrid = L.geoJson(data);
+        fetch(grid)
+            .then(response => response.json())
+            .then(data => {
+                // Vide la couche de données (elle peut être vide avant cette étape)
+                geoJsonGrid.clearLayers();
+                // Rajoute les nouvelles données à la couche
+                geoJsonGrid.addData(data);
+            });
     }
     //Vérifie si le bouton buffer est coché
     else if (document.getElementById('buffer').checked) {
         // Récupération de la grille avec buffer
-        const bufferedResponse = await fetch(bufferedGrid);
-        const bufferedData = await bufferedResponse.json();
-        geoJsonGrid = L.geoJson(bufferedData);
-        //création d'un buffer négatif pour conserver une lisibilité de l'information cartographique
-        var geoJsonGridBuffered = turf.buffer(geoJsonGrid.toGeoJSON(), -0.05, { units: 'kilometers' });
-        var leafletGridBuffered = L.geoJSON(geoJsonGridBuffered);
-        geoJsonGrid = leafletGridBuffered;
+        fetch(bufferedGrid)
+            .then(bufferedResponse => bufferedResponse.json())
+            .then(bufferedData => {
+                // Vide la couche de données (elle peut être vide avant cette étape)
+                geoJsonGrid.clearLayers();
+                // Retrait visuel du buffer, qui ne modifie pas les données de la grille avec buffer
+                var unbufferedData = turf.buffer(bufferedData, -0.05, { units: 'kilometers' });
+                // Rajoute les nouvelles données à la couche
+                geoJsonGrid.addData(unbufferedData);
+            });
     }
-    if (document.getElementById('gradient').checked) {
-        L.geoJSON(geoJsonGrid.toGeoJSON(), { style: shadedStyle }).addTo(mapGrid);
-    }
-    else if (document.getElementById('binary').checked) {
-        L.geoJSON(geoJsonGrid.toGeoJSON(), { style: binaryStyle }).addTo(mapGrid);
-    }
-    mapGrid.addTo(mymap)
 }
 
-function clearAllLayers() {
-    for (var i = 0; i < mapGrid.length; i++) {
-        mymap.removeLayer(mapGrid[i]);
-    }
-    //this line empties the array
-    mapGrid.length = 0;
-}
-
-mymap.on('click', function () {
-    clearAllLayers()
-    //mymap.removeLayer(mapGrid);
-    //var mapGrid = L.layerGroup()
-    getGrid()
-});
+// Appelle la fonction getGrid sur un click, zoom ou déplacement
+mymap.on('moveend', getGrid);
 
 var radiosOui = document.getElementsByName('ouinon');
 var radiosMode = document.getElementsByName('mode');
@@ -141,5 +123,3 @@ for (i = 0; i < radiosMode.length; i++) {
         }
     }
 }
-
-//toLoadGrid(grid);
